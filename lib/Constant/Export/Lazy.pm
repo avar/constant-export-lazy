809aaf2af5fec80faa1adac34ab90971e23050d2
Neil Bowers <neil@bowers.com>
2014-12-07 00:16:27 +0000

    Added COPYRIGHT AND LICENSE section to doc

--- lib/Constant/Export/Lazy.pm
+++ lib/Constant/Export/Lazy.pm
@@ -931,5 +931,12 @@ there's no stash reference defined.
 
 Ævar Arnfjörð Bjarmason <avar@cpan.org>
 
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Ævar Arnfjörð Bjarmason <avar@cpan.org>
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 =cut
 
